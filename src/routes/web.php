b07a14d0c4ec07dabca111bd1f8fe79a9bdd7af4
Morya Samyak Arrais Benício <ayromkaymas@gmail.com>
2025-08-29 16:40:22 -0300

    Add files via upload

---
+++ src/routes/web.php
@@ -0,0 +1,26 @@
+<?php 
+
+use Slim\App;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Src\Controller\TransationController;
+
+return function(App $app) {
+
+     $app->post('/transacao', [TransationController::class, 'insert']);
+
+     $app->get('/transacao/{id}', [TransationController::class, 'DataReturnById']);
+
+     $app->delete('/transacao/{id}', [TransationController::class, 'dataDeleteById']);
+
+     $app->delete('/transacao', [TransationController::class, 'dataDelete']);
+
+     $app->get('/estatistica', [TransationController::class, 'calculateStatistics']);
+     
+     $app->map(['GET', 'POST','DELETE'], '/{routes:.+}', function ($request, $response) { 
+          return $response->withStatus(404);
+     });     
+
+}
+
+?>
